790f4647b5dc1e81bed02bd7da0b4891baf25e7f
Antar1011 <summer.kenster@gmail.com>
2020-06-14 17:15:11 -0400

    Code fixes and documentation

--- ShardIllumination.ino
+++ ShardIllumination.ino
@@ -1,4 +1,36 @@
-const int color_palette[9][3] = {{255,   0,   0}, // red
+/*
+  ShardIllumination
+
+  Code to control an RGB light strip (single-color, not individually
+  addressible) that is used to illuminate a shard of acrylic glass.
+
+  An Arduino Nano was used as the controller for this project,
+  as the ESP8266 only has one analog-in pin, but an alternative
+  version of this project would replace the potentiometer knobs
+  with Wi-Fi controls (read: Blynk app).
+
+  The circuit:
+  * 3 2N2222 BJTs
+  * 3 10kΩ linear rotary potentiometers
+  * 3 220Ω resistors
+  * 1 MT3608 boost converter with Micro USB input (the Micro
+    USB port is optional, as you could always power the circuit
+    through the Arduino Nano)
+  * 1 short (3 light) strip of SMD 5050 RGB LEDs
+
+  All components I selected were compatible with either 5V
+  or 3.3V logic, so that should give you some freedom as well.
+
+  Created 14 June 2020
+  By Gili (OpenBagTwo) Barlev
+
+  http://url/of/online/tutorial.cc
+
+*/
+
+// set constants
+const int color_palette[9][3] = {                 // colors to cycle through in demo mode
+                                 {255,   0,   0}, // red
                                  {255, 117,  56}, // orange
                                  {255, 255,  51}, // yellow
                                  {  0, 255,   0}, // green
@@ -7,27 +39,32 @@ const int color_palette[9][3] = {{255,   0,   0}, // red
                                  {148,   0, 211}, // violet
                                  {255, 255, 255}, // white
                                  {  0,   0,   0}, // black
-                                };
+                                };                
 
-const int rgb_pins[3] = {3, 5, 6};
-const int color_control_pins[3] = {A0, A1, A2};
-const int push_button = 1;
+const int animation_wait_time = 500;              // time between frames of the idle / demo animation
 
-const int animation_wait_time = 500;
+// set pins
+const int rgb_pins[3] = {3, 5, 6};                // LED output pins (R, G, B)
+const int color_control_pins[3] = {A0, A1, A2};   // controller pins (R, G, B)
+const int push_button = 1;                        // tactile push button
 
-int iter;
-bool demoMode;
-unsigned long lastPress;                       // last time the button was pressed (in ms since bootup)
+// global variables
+int iter;                                         // frame number of the idle / demo animation
+bool demoMode;                                    // whether to deplay demo sequence (true) or set color based
+                                                  // on controller values
+unsigned long lastPress;                          // last time the button was pressed (in ms since bootup)
 
 void setup() {
+  Serial.begin(9600);
+  iter = 0;
+  demoMode = true;
+  lastPress = 0;
+  
   for (int i=0; i<3; i++){
     pinMode(rgb_pins[i], OUTPUT);
     pinMode(color_control_pins, INPUT);
   }
   pinMode(push_button, INPUT);
-  iter = 0;
-  demoMode = true;
-  lastPress = 0;
 }
 
 void loop() {
@@ -63,10 +100,18 @@ void displayDemo() {
  * Set the color based on the controller input values 
  * 
  */
-void displayDemo() {
+void displayFromControls() {
+  String message = "(";
   for (int i=0; i<3; i++){
-    analogWrite(rgb_pins[i], analogRead(color_control_pins[i]) / 4);
+    int color_value = analogRead(color_control_pins[i]) / 4;
+    message += sprintf("%03d", color_value);
+    if (i<3){
+      message += ",";
+    }
+    analogWrite(rgb_pins[i], color_value);
   }
+  message += ")";
+  Serial.println(message);
 }
 
 
